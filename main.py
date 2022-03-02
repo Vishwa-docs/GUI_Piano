aaaff03f90ca47b8cf6c18861f4f3e57ee3c482a
Vishwa Kumaresh <vishwakumaresh@gmail.com>
2022-03-02 09:05:59 +0530

    Created the Initial file

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+#MYSPACE
+Change Font and Background Colour
+Check online Notes Importing
+see if you can make it a key input (if event.type == pygame.KEYDOWN and event.key == pygame.K_a:)
+'''
+
+
+#NOTES FILE : https://drive.google.com/file/d/0B5VO9Z-lZzRYSncwNGhmWmhHenc/view
+
+#CODE
+from tkinter import*
+import time
+import datetime
+import pygame
