a5cf5b51254e9b6bc3c0566a44dcbf3d2ade4afe
Vishwa Kumaresh <vishwakumaresh@gmail.com>
2022-03-05 20:43:45 +0530

    Added Keys

--- main.py
+++ main.py
@@ -32,3 +32,34 @@ ABC3=Frame(ABC, bg="powder blue", relief= RIDGE)
 ABC3.grid()
 
 
+#Sharp FUNCTIONS (Try playsound) (Check importing from online source)
+def value_Cs():
+    str1.set("C#")
+    sound = pygame.mixer.Sound ("C:\\Users\\Student\\Desktop\\Comp Project V\\Piano notes\\C_s.wav") #Check Pathways
+    sound.play()
+        
+def value_Ds():
+    str1.set("D#")
+    sound = pygame.mixer.Sound ("C:\\Users\\Student\\Desktop\\Comp Project V\\Piano notes\\D_s.wav") #Check Pathways
+    sound.play()
+def value_Fs():
+    str1.set("F#")
+    sound = pygame.mixer.Sound ("C:\\Users\\Student\\Desktop\\Comp Project V\\Piano notes\\F_s.wav") #Check Pathways
+    sound.play()    
+def value_Gs():
+    str1.set("G#")
+    sound = pygame.mixer.Sound ("C:\\Users\\Student\\Desktop\\Comp Project V\\Piano notes\\G_s.wav") #Check Pathways
+    sound.play()
+def value_Bb():
+    str1.set("Bb")
+    sound = pygame.mixer.Sound ("C:\\Users\\Student\\Desktop\\Comp Project V\\Piano notes\\Bb.wav") #Check Pathways
+    sound.play()    
+def value_Cs1():
+    str1.set("Cs1")
+    sound = pygame.mixer.Sound ("C:\\Users\\Student\\Desktop\\Comp Project V\\Piano notes\\C_s1.wav") #Check Pathways
+    sound.play()    
+def value_Ds1():
+    str1.set("Ds1")
+    sound = pygame.mixer.Sound ("C:\\Users\\Student\\Desktop\\Comp Project V\\Piano notes\\D_s1.wav") #Check Pathways
+    sound.play()
+    
